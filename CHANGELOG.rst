b2e6ede8ec3bfc7fdc197c0ff9c8941d84f4ef7d
Rémy HUBSCHER <remy@chefclub.tv>
2019-04-04 09:48:53 +0200

    Update README.

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -3,7 +3,7 @@ Changelog
 
 This document describes changes in the installer between each past release.
 
-0.8.1.0 (2019-04-04)
+0.8.1.1 (2019-04-04)
 --------------------
 
 - Create a setup.py that allow for install.
--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ with codecs.open(os.path.join(HERE, "CONTRIBUTORS.rst"), encoding="utf-8") as f:
 
 setup(
     name="elm-format",
-    version="0.8.1.0",
+    version="0.8.1.1",
     description="elm-format installer",
     long_description=README + "\n\n" + CHANGELOG + "\n\n" + CONTRIBUTORS,
     license="Apache License (2.0)",
